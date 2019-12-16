cf756ab1856ece3d8520a6cead0186367c24242d
Sergio Suárez <triplesxulo@gmail.com>
2019-12-16 09:45:03 +0100

    gnf

--- reg curso.php
+++ reg curso.php
@@ -7,13 +7,6 @@
 			<p>Registro de curso</p>
                 <input type="text" placeholder="Código Curso" name="codigo"/>
                 <input type="text" placeholder="Nombre" name="nombre"/>
-                <input type="radio" name="radio3" value="1">1ºASIR
-                <input type="radio" name="radio3" value="2" checked>2ºASIR
-            <p>
-                <input type="button" name="aceptar" value="Aceptar">
-                <input type="button" name="cancelar" value="Cancelar">
-                <input type="submit" name="atras" value="Atrás" />
-            </p>
         </form>
     </BODY>
  </HTML>
